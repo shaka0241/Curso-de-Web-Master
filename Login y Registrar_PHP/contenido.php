ff021b623e25debde6bb9e4dc957322dff616000
ROJASAEU <alrojas@mpetromin.gob.ve>
2017-05-15 18:58:14 -0400

    renombradas varias carpetas

---
+++ Login y Registrar_PHP/contenido.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Contenido</title>
+</head>
+<body>
+   
+   <h1>BIENVENIDO!!!!!</h1>
+   
+   <a href="logout.php">Cerrar Sesion</a>
+    
+</body>
+</html>
--- login_php_curso/logout.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-session_start();
-
-session_destroy();
-
-print"<script>location='index.php'</script>";
-
-?>
